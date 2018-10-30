1c55b6a7a39da2548744786ca7588852874e93b9
Francisco Sanchez <fsancheztemprano@danielcastelao.org>
2018-10-30 10:38:03 +0100

    Creacion de comentarios para JavaDoc fix imprecisiones con isDivisible3

--- numerosprimos/NumerosPrimos.java
+++ numerosprimos/NumerosPrimos.java
@@ -1,14 +1,17 @@
 package numerosprimos;
 
 /**
+ * Calcular numeros primos entre 2 numeros determinados (startNum, finalNum)
  * @author fsancheztemprano
+ * @version 0.2
+ * 
  */
 public class NumerosPrimos {
 
     public static int startNum = 1;
     public static int activeNum = startNum;
-    public static int finalNum = 99929;
-    public static int numOfPrimes = 0;
+    public static int finalNum = 100;  
+    public static int numOfPrimes = 0;         
     public static int numOfNotPrimes = 0;
     public static int numOfSkips;
     public static int numOfSkips3 = 0;
@@ -29,7 +32,7 @@ public class NumerosPrimos {
         for (int i = activeNum; i <= finalNum; i += 2) {
             int count;
 
-            if ((activeNum <= 3) || isEven(activeNum)) {
+            if ((activeNum < 3) || isEven(activeNum)) {
                 activeNum++;
                 i--;
             } else {
@@ -39,32 +42,24 @@ public class NumerosPrimos {
             if (activeNum > finalNum) {
                 break;
             }
-            if (activeNum >= 3 && isDivisible3(activeNum)) { //useless code redundant on 6
+            if (activeNum > 3 && isDivisible3(activeNum)) {
                 numOfSkips3++;
-                if (debug) {
-                    System.out.println(activeNum + " Skip3++");
-                }
+                if (debug) System.out.println(activeNum + " Skip3++");
                 continue prime;
             }
             if (activeNum >= 6 && isDivisible6(activeNum)) {
                 numOfSkips6++;
-                if (debug) {
-                    System.out.println(activeNum + " Skip6++");
-                }
+                if (debug)  System.out.println(activeNum + " Skip6++");
                 continue prime;
             }
             if (activeNum >= 31 && isDivisible30(activeNum)) {
                 numOfSkips30++;
-                if (debug) {
-                    System.out.println(activeNum + " Skip30++");
-                }
+                if (debug) System.out.println(activeNum + " Skip30++");                
                 continue prime;
             }
             if (activeNum >= 210 && isDivisible210(activeNum)) {
                 numOfSkips210++;
-                if (debug) {
-                    System.out.println(activeNum + " Skip210++");
-                }
+                if (debug) System.out.println(activeNum + " Skip210++");
                 continue prime;
 
             }
@@ -86,7 +81,10 @@ public class NumerosPrimos {
         System.out.println(calcsDone + " Loops");
 
     }
-
+    /**
+     * cuenta la cantidad de divisores enteros del actualNum hasta alcanzar 3 divisores enteros
+     * @return devuelve la cantidad de divisores enteros del actualNum 
+     */
     public static int countDivisors() {
         int count = 0;
         int actualNum = activeNum;
@@ -106,7 +104,12 @@ public class NumerosPrimos {
         }
         return count;
     }
-
+/**
+ * este metodo actua sobre un numero que ya ha sido determinado primo
+ * aumenta numOfPrimes para llevar una cuenta de cuantos primos hemos conseguido
+ * llama a leapPrime 
+ * imprime por consola el numero primo 
+ */
     public static void isPrime() {
         numOfPrimes++;
         leapPrime();
@@ -117,21 +120,32 @@ public class NumerosPrimos {
             System.out.println("Number: " + activeNum + " IS prime! " + "" + (activeNum % 6) + " " + (activeNum % 30) + " " + (activeNum % 210));
         }
     }
-
+/**
+ * este metodo actua sobre un numero que ya ha sido determinado primo
+ * aumenta numOfPrimes para llevar una cuenta de cuantos primos hemos conseguido
+ * llama a leapPrime 
+ * imprime por consola el numero primo 
+ */
     public static void isNotPrime() {
         numOfNotPrimes++;
         if (notPrimes) {
             System.out.println("Number: " + activeNum + " NOT prime! " + "" + (activeNum % 6) + " " + (activeNum % 30) + " " + (activeNum % 210));
         }
     }
-
+/**
+ * lleva una cuenta de la distancia entre numeros primos consecutivos  para determinar los mas distanciados
+ */
     public static void leapPrime() {
         if ((activeNum - lastPrime) > bigestLeap) {
             bigestLeap = activeNum - lastPrime;
             bigestLeaper = activeNum;
         }
     }
-
+/**
+ * Comprueba si el numero es Par
+ * @param n numero a comprobar
+ * @return true si el numero es par
+ */
     public static boolean isEven(int n) {
         if (n % 2 == 0) {
             return true;
@@ -139,7 +153,11 @@ public class NumerosPrimos {
             return false;
         }
     }
-
+/**
+ * comprueba si el numero introducido "n" es divisible entre 3
+ * @param n numero a comprobar divisibilidad
+ * @return true si es divisible entre 3
+ */
     public static boolean isDivisible3(int n) {
         if (n % 3 != 0) {
             return false;
@@ -147,7 +165,11 @@ public class NumerosPrimos {
             return true;
         }
     }
-
+/**
+ * Comprueba si, al dividir numero introducido "n" entre 6 el modulo es un numero primo
+ * @param n numero a comprobar divisibilidad
+ * @return true si el resultado n % 6 no es primo
+ */
     public static boolean isDivisible6(int n) {
         if (n % 6 != 1 && n % 6 != 5) {
             return true;
@@ -155,7 +177,11 @@ public class NumerosPrimos {
             return false;
         }
     }
-
+/**
+ * Comprueba si, al dividir numero introducido "n" entre 30 el modulo es un numero primo
+ * @param n numero a comprobar divisibilidad
+ * @return true si el resultado n % 30 no es primo
+ */
     public static boolean isDivisible30(int n) {
         if (n % 30 != 7 && n % 30 != 11 && n % 30 != 13 && n % 30 != 23 && n % 30 != 27 && n % 30 != 29) {
             return true;
@@ -163,7 +189,12 @@ public class NumerosPrimos {
             return false;
         }
     }
-
+/**
+ * Comprueba si, al dividir numero introducido "n" entre 210 el modulo es un numero primo
+ * @param n numero a comprobar divisibilidad
+ * @return true si el resultado n % 210 no es primo
+ */
+    
     public static boolean isDivisible210(int n) {
         if (n % 210 != 31
                 && n % 210 != 37
@@ -207,7 +238,12 @@ public class NumerosPrimos {
         }
     }
 
-    //TODO
+/**
+ * EN CONTRUCCION
+ * Comprueba si, al dividir numero introducido "n" entre 2310, el modulo es un numero primo
+ * @param n numero a comprobar divisibilidad
+ * @return true si el resultado n % 210 no es primo
+ */
     public static boolean isDivisible2310(int n) {
         return true;
     }
